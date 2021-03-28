28051a332a22b154d6b9ea430ee60b978a1d82b3
Yutaro Yoshii <yakumodanshi@gmail.com>
2021-03-29 01:49:23 +0900

    completed

--- main.js
+++ main.js
@@ -5,92 +5,141 @@ canvas.width = width;
 canvas.height = height;
 var ctx = canvas.getContext("2d");
 
-var RandomWalk = function(n){
-    var points = [];
-    for(var i = 0; i < n; i++){
-        points.push(0);
-    };
+var Pascal = function(){
+    var ta = [1];
+    var odd = true;
+    var n = 1;
     this.step = function(){
-        for(var i = 0; i < n; i++){
-            points[i] += Math.random()*2-1;
+        if(odd){
+            for(var i = 0; i < ta.length-1; i++){
+                ta[i] = ta[i]+ta[i+1];
+            }
+        }else{
+            var pv = ta[0];
+            ta[0] += ta[0];
+            for(var i = 1; i < ta.length; i++){
+                var pv0 = pv;
+                pv = ta[i];
+                ta[i] = pv0+ta[i];
+            }
+            ta[ta.length] = 1/(2**(ta.length*2));
         }
-    };
+        odd = !odd;
+        //counting n
+        var n0 = n;
+        n = 0;
+        if(odd){
+            n += ta[0];
+            for(var i = 1; i < ta.length; i++){
+                n += ta[i]*2;
+            }
+        }else{
+            for(var i = 0; i < ta.length; i++){
+                n += ta[i]*2;
+            }
+        }
+        for(var i = 0; i < ta.length; i++){
+            ta[i] = ta[i]*n0/n;
+        }
+        n = n0;
+        return ta;
+    }
+    this.t = ta;
     this.getAvg = function(){
-        var sum = 0;
-        for(var i = 0; i < n; i++){
-            sum += points[i];
-        }
-        return sum/n;
-    };
-    var that = this;
-    this.getStandardDeviation = function(avg){
+        return 0;//yes, it's that simple
+    }
+    this.getStandardDeviation = function(){
         //look at this page https://en.wikipedia.org/wiki/Standard_deviation
         var sumSquaredDeviation = 0;
-        for(var i = 0; i < n; i++){
-            sumSquaredDeviation += (points[i]-avg)*(points[i]-avg);
+        if(odd){
+            sumSquaredDeviation += 0*0*ta[0];
+            for(var i = 1; i < ta.length; i++){
+                sumSquaredDeviation += i*i*ta[i]*2;
+            }
+        }else{
+            for(var i = 0; i < ta.length; i++){
+                sumSquaredDeviation += (i+0.5)*(i+0.5)*ta[i]*2;
+            }
         }
         return Math.sqrt(sumSquaredDeviation/n)
     };
-    this.getCenterProbabilityDensity = function(avg,sd){
-        //area with 0.1 standard deviation
-        var cntCenterStrip = 0;
-        for(var i = 0; i < n; i++){
-            if(Math.abs(points[i]-avg) < 0.1*sd){
-                cntCenterStrip++;
-            }
-        }
-        var centerDencity = cntCenterStrip/n/(0.1*sd*2);
-        return centerDencity;
-    }
-    this.getPI = function(avg,sd,pd){
+    this.getCenterProbabilityDensity = function(){
+        return ta[0]/n;
+    };
+    this.getPI = function(sd,pd){
         return 1/(sd*sd*2*pd*pd);
     };
-    this.getDistributionArray = function(width,avg,sd){//n is the width
+    this.getDistributionArray = function(width,avg,sd){
         //3 sigmas
         var arr = [];
         for(var i = 0; i < width; i++){
             arr[i] = 0;
         }
-        for(var i = 0; i < n; i++){
-            var val = (points[i]-avg+sd*3);
+        var addToArr = function(pos,len,content){
+            var avg = (len-1)/2;
+            var val = (pos-avg+sd*3);
             var idx = Math.floor(val/(sd*6)*width);
-            if(idx < 0 || idx >= width)continue;
-            arr[idx]++;
+            if(idx < 0 || idx >= width)return false;
+            arr[idx] += content;
+        };
+        
+        if(odd){
+            var len = 1+(ta.length-1)*2;
+            addToArr((ta.length-1),len,ta[0]);
+            for(var i = 1; i < ta.length; i++){
+                addToArr((ta.length-1)+i,len,ta[i]);
+                addToArr((ta.length-1)-i,len,ta[i]);
+            }
+        }else{
+            var len = ta.length*2;
+            for(var i = 0; i < ta.length; i++){
+                addToArr((ta.length-1)-i,len,ta[i]);
+                addToArr((ta.length)+i,len,ta[i]);
+            }
         }
         return arr;
     }
 };
 
-var N = 1000000;
-var rw = new RandomWalk(N);
+var pa = new Pascal();
 
 var start = 0;
 var animate = function(t){
     if(start === 0)start = t;
     var dt = t - start;
     start = t;
-    rw.step();
-    var avg = rw.getAvg();
-    var sd = rw.getStandardDeviation(avg);
-    var pd = rw.getCenterProbabilityDensity(avg,sd);
-    var pi = rw.getPI(avg,sd,pd);
+    pa.step();
+    console.log("step");
+    var avg = pa.getAvg();
+    var sd = pa.getStandardDeviation(avg);
+    console.log(sd);
+    var pd = pa.getCenterProbabilityDensity();
+    console.log(pd);
+    var pi = pa.getPI(sd,pd);
     console.log(pi);
     document.getElementById("display").innerHTML = "π = "+pi;
     var w = 500;
-    var arr = rw.getDistributionArray(w,avg,sd);
-    drawDistributionArray(arr,w,N);
+    var arr = pa.getDistributionArray(w,avg,sd);
+    drawDistributionArray(arr,w);
     requestAnimationFrame(animate);
 }
 requestAnimationFrame(animate);
 
 
 
-var drawDistributionArray = function(arr,w,N){
+var drawDistributionArray = function(arr,w){
+    var maxVal = 0;
+    for(var i = 0; i < arr.length; i++){
+        if(maxVal < arr[i]){
+            maxVal = arr[i];
+        }
+    }
+    
     ctx.clearRect(0,0,width,height);
     var imgdata = ctx.getImageData(0,0,width,height);
     var data = imgdata.data;
     for(var i = 0; i < w; i++){
-        var level = arr[i]/(N/w/100);
+        var level = arr[i]/maxVal*300;
         if(level > canvas.height-1){
             level = canvas.height-1;//trim the upper end so we dont go into the undefined rterritory
         }
